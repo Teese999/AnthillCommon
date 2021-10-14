806ea44c3124f7297e6d27f002b50a101a560c35
osipenkom <osipenkom@gmail.com>
2021-10-14 17:06:53 +0300

    application operational now

---
+++ AnthillCommon/ContainerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unity;
+using Unity.Lifetime;
+
+namespace AnthillCommon
+{
+    public class ContainerConfiguration
+    {
+        public static void RegisterTypes<TLifetime>(IUnityContainer container, IConfiguration configuration)
+           where TLifetime : ITypeLifetimeManager, new()
+        {
+            Services.ContainerConfiguration.RegisterTypes<HierarchicalLifetimeManager>(container, configuration);
+        }
+    }
+}
